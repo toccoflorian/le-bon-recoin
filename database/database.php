2e53a1ea410eab5276611b788115f874a575480f
Florian <t.florian181181@gmail.com>
2023-08-29 16:01:35 +0200

    simple detail

--- database/database.php
+++ database/database.php
@@ -1,9 +1,9 @@
 <?php
 
-
+$DBPassword = getenv($DBPassword);
 
 try {
-    $pdo = new PDO('mysql:host=127.0.0.1:3306;dbname=e-commerce;', 'root', '45222254');
+    $pdo = new PDO('mysql:host=127.0.0.1:3306;dbname=e-commerce;', 'root', $DBPassword);
 } catch (PDOException $error) {
     print_r($error->getMessage());
 }
